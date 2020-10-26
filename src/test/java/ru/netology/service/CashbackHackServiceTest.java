a886242407070077c7c9b3491511e9719c3f23ba
Nikoly <turnik3580@gmail.com>
2020-10-26 12:48:04 +0300

    add project

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService ();
+
+    @Test
+    public void shouldCashBackPlus() {
+        assertEquals(service.remain(900), 100);
+    }
+
+    @Test
+    public void shouldCashBackBoundaryMoreThan1000() {
+        assertEquals(service.remain(1001), 999);
+    }
+
+    @Test
+    public void shouldCashBackBoundaryLessThan1000() {
+        assertEquals(service.remain(999), 1);
+    }
+
+    @Test
+    public void shouldCashBackNoMore() {
+        assertEquals(service.remain(1000), 0);
+    }
+}
+
